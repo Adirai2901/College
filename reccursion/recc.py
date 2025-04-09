44f77e4a88cda28f06996aa19b8b714a48d604a8
Aditya Rai <adirai2901@gmail.com>
2025-04-09 09:08:01 +0530

    Python

---
+++ reccursion/recc.py
@@ -0,0 +1,7 @@
+def fib(n):
+    if n <= 1:return (n)
+    return fib(n-1) +fib(n-2)   
+
+print(fib(10))
+#step 4 from all the sol from all the  sub program combining al the solution together 
+#it works on the prici on div and conc
